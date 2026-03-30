f136677bc02fca2b3aa48f34d205cbf0e9abba9f
moe19 <moe19@i.softbank.jp>
2026-03-30 14:51:29 +0900

    検索の修正

--- app/Searchs/SelectNameDetails.php
+++ app/Searchs/SelectNameDetails.php
@@ -27,12 +27,33 @@ class SelectNameDetails implements DisplayUsers{
     ->where(function($q) use ($role, $gender){
       $q->whereIn('sex', $gender)
       ->whereIn('role', $role);
-    })
-    ->whereHas('subjects', function($q) use ($subjects){
-      $q->where('subjects.id', $subjects);
-    })
-    ->orderBy('over_name_kana', $updown)->get();
-    return $users;
-  }
+    });
 
+    // ↓元記述
+    // whereInだと複数検索の時に1つでも当てはまるユーザーをすべて表示されてしまう
+    // ->whereHas('subjects', function($q) use ($subjects){
+    //   $q->where('subjects.id', $subjects);
+    // })
+
+    if($subjects){
+      foreach($subjects as $subject_id){
+        $users->whereHas('subjects',function($q) use ($subject_id){
+          $q->where('subjects.id', $subject_id);
+        });
+      }
+    }
+    // ↑foreachとwhereHasを一緒に記述してAND検索(且つ)を実装。
+    //  複数検索するときにすべてに当てはまるユーザーを表示させる
+    $searchResults = $users->orderBy('over_name_kana', $updown)->get();
+    return $searchResults;
+  }
 }
+
+
+// ☆where句
+// where:「且つ」/A且つB
+// whereHas:リレーション先(中間テーブル)のテーブルで絞り込み
+//          DBではなくリレーション先から髭右得したいデータを持ってくる
+
+// whereIn:「どれか」/A,B,Cのどれか
+// orWhere:「または」/どちらか1つ(AまたはB)
