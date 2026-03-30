ac99f5979f4ec4d0f8f79071103bd037c4491314
moe19 <moe19@i.softbank.jp>
2026-03-30 13:26:00 +0900

    レイアウト調整

--- app/Calendars/General/CalendarWeekDay.php
+++ app/Calendars/General/CalendarWeekDay.php
@@ -49,7 +49,10 @@ class CalendarWeekDay{
      }
 
      $html = [];
-     $html[] = '<select name="getPart[]" class="border-primary" style="width:50px; border-radius:5px;" form="reserveParts">';
+    //  $html[] = '<select name="getPart[]" class="border-primary" style="width:70px; border-radius:5px;" form="reserveParts">';
+    // ↑元の記述
+    $html[] = '<select name="getPart[]" class="border-primary" style="width:50px; border-radius:5px;" form="reserveParts">';
+
      $html[] = '<option value="" selected></option>';
      if($one_part_frame == "0"){
        $html[] = '<option value="1" disabled>リモ1部(残り0枠)</option>';
